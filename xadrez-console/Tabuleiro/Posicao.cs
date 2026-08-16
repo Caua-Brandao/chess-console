3ffacebdddd583063f05eee49b81671e62527c85
Caua <cauabrandaocosta1503@gmail.com>
2026-08-16 09:42:01 -0300

    Criando a classe posição

---
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿namespace Tabuleiro
+{
+    internal class Posicao
+    {
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+    }
+}
